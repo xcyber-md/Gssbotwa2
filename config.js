10cdcba2dcb0773d0b2aa7d242c3d7105f7b66da
XLICON-V3-MD <165268074+xcyber-md@users.noreply.github.com>
2024-07-21 11:11:07 -0400

    Update config.js

--- config.js
+++ config.js
@@ -3,29 +3,29 @@ const chalk = require('chalk');
 
 
 // Other
-global.link = 'https://whatsapp.com/channel/0029VaWJMi3GehEE9e1YsI1S'
-global.linkGroup = 'https://chat.whatsapp.com/E3PWxdvLc7ZCp1ExOCkEGp'
+global.link = 'https://whatsapp.com/channel/0029VaBYxg0FCCoPEa0cP53U'
+global.linkGroup = 'https://whatsapp.com/channel/0029VaBYxg0FCCoPEa0cP53U'
 
 global.apikey = 'bf2d2cf29b3edc604b447983';
-global.ownername = "𝑆𝛪𝐷 𝐵𝛨𝛥𝛪";
-global.owner = ['917050906659'];
-global.premium = ['917050906659'];
+global.ownername = "cyber rivet";
+global.owner = ['923145283560'];
+global.premium = ['923145283560'];
 global.packname = "𝚜𝚝𝚒𝚌𝚔𝚎𝚛 𝚋𝚢";
-global.author = "𝐆𝐒𝐒 𝚩𝚯𝚻𝐖𝚫";
+global.author = "cyber rivet";
 global.sessionName = 'session';
 global.prefa = [".", "!", "/", "?"];
 global.typemenu = "v1"
 global.menuType = "2"
 global.autoswview = true
-global.autoread = false
-global.available = false
-global.autoTyping = false
-global.autoRecord = false
-global.autoreact = false
-global.onlygroup = false
-global.onlypc = false
+global.autoread =  true
+global.available = true
+global.autoTyping =  true
+global.autoRecord =  true
+global.autoreact =  true
+global.onlygroup =  false
+global.onlypc =  false
 global.welcome = true
-global.autoBlock = true
+global.autoBlock = false
  
 /*REPLY MESSAGE*/
 global.mess = {
